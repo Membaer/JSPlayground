22a5b8c120442db555ab51043200a0fe135fab40
Olivia Schütt <olivia.schuett@gmail.com>
2014-02-16 19:35:25 +0100

    Playground JS

---
+++ Playground/Playground/js/Schleife.js
@@ -0,0 +1,37 @@
+﻿function machcanvas() {
+    var x = 0;
+    var y = 0;
+    var malerei = document.getElementsByTagName("canvas")[0].getContext('2d');
+    for (i = 0; i < 3; i++) {
+        for (k = 0; k < 3; k++) {
+        //console.debug(malerei);
+            malerei.fillStyle = "rgba(255,0,0,0.2)";
+            malerei.fillRect(x, y, 50, 50);
+            malerei.fillStyle = "rgba(0,255,0,0.3)";
+            malerei.fillRect(x + 15, y + 15, 20, 80);
+            y += 100;
+        }
+        x += 60;
+        y = 0;
+    }
+}
+
+//window.onload = machcanvas;
+
+function checkinput() {
+    var inputfields = document.getElementsByTagName("input")
+    for (i = 0; i < inputfields.length; i++) {
+        if (inputfields[i].value == "") {
+            alert("Bitte ausfüllen");
+            inputfields[i].focus();
+            break;
+        }
+        else {
+        }
+    }
+}
+
+window.onload = function ()
+{
+    document.getElementById("mybtn").onclick = checkinput;
+}
---
+++ Playground/Playground/js/canvas.js
@@ -0,0 +1,18 @@
+﻿
+
+
+
+function zeichnen() {
+    var malerei = document.getElementsByTagName("canvas")[0].getContext('2d');
+    //console.debug(malerei);
+    malerei.fillStyle = "rgba(255,0,0,0.2)";
+    malerei.fillRect(0, 0, 140, 80);
+    malerei.fillStyle = "rgba(0,255,0,0.3)";
+    malerei.fillRect(15, 15, 60, 130);
+}
+
+function init() {
+    zeichnen();
+}
+
+window.onload = init;
